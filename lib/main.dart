453afb1c92656ebe168b3396019348ae21e0e86f
goomin <rkdalsrna@gachon.ac.kr>
2026-09-15 16:39:07 +0900

    feat: 시작 화면 UI 및 MovieLog 로고 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'theme/app_theme.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(const MovieLogApp());
 
@@ -33,7 +34,12 @@ class StartScreen extends StatelessWidget {
                   const SizedBox(height: 32),
                   const Text('FLUTTER 1주차'),
                   const SizedBox(height: 64),
-                  Icon(Icons.movie_outlined, size: 72, color: colors.primary),
+                  SvgPicture.asset(
+                    'assets/logos/movielog_logo.svg',
+                    width: 72,
+                    height: 72,
+                    semanticsLabel: 'MovieLog 로고',
+                  ),
                   const SizedBox(height: 32),
                   Text(
                     '영화의 순간을\n기록하세요',
